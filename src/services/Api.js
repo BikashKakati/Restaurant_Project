14d7d237074705cd505cc88dab2665dde513692a
Bikash Kakati <bikashkakati796@gmail.com>
2024-01-20 23:01:57 +0530

    Feature - Started integrating DB

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { db } from "./firebase-config";
+import { doc, setDoc } from "firebase/firestore";
 
 const BASE_URL = "https://www.themealdb.com/api/json/v1/1/";
 export async function fetchApiData(endPoint){
@@ -9,4 +11,11 @@ export async function fetchApiData(endPoint){
         console.log(err);
     }
 }
-
+export async function setCartDetails(userId,expenseData){
+    try{
+        const docRef = doc(db,`currentUser/${userId}/expense`,expenseData.id);
+        await setDoc(docRef, expenseData);
+    }catch(err){
+        throw new Error(err.message);
+    }
+}
