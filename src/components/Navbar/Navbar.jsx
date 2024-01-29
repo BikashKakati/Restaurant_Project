0e0c3385caa35283e58501cf0106c8bd35aa29f6
Bikash Kakati <bikashkakati796@gmail.com>
2024-01-29 10:54:04 +0530

    Style - Change navbar color
    Navbar color change in different route

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,19 @@
 import { UserCircleIcon } from "@heroicons/react/24/outline";
 import { ShoppingCartIcon } from "@heroicons/react/24/solid";
 import { PhoneIcon } from "@heroicons/react/24/solid";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import Wrapper from "../Ui/Wrapper";
 import { useSelector } from "react-redux";
 
 function Navbar() {
+    const location = useLocation();
     const { currentUser } = useSelector(state => state.auth);
     const { cartDetails } = useSelector(state => state.cart);
     const mealsQuantity = cartDetails?.reduce((initialQantity, cartMeal) => {
         return initialQantity += cartMeal.quantity;
     }, 0)
     return (
-        <nav className="w-full py-1 bg-zinc-800 absolute block top-0 left-0 z-20">
+        <nav className={`w-full py-0.5 absolute ${location.pathname ==='/' ? "bg-nav":"bg-zinc-800"} block top-0 left-0 z-20`}>
             <Wrapper className="flex items-center justify-between text-white">
                 <NavLink to={"/"}>
                     <div className="px-6 py-3 md:hover:nav-hover cursor-pointer transition-all">
@@ -21,11 +22,11 @@ function Navbar() {
                 </NavLink>
 
                 <ul className="fixed left-0 bottom-0 w-full  bg-zinc-800 md:relative md:w-auto md:bg-transparent flex items-center justify-around  md:justify-center md:space-x-6 font-medium transition-all">
-
+                    {currentUser && <li>Welcome {currentUser?.email}</li>}
                     <li className="px-5 py-3 md:hover:nav-hover">
                         {
                             currentUser ?
-                                <Link to="/profile" className="flex flex-col">
+                                <Link to="/profile" className="flex items-center flex-col">
                                     <UserCircleIcon className="h-7 w-7" />
                                     <span className="text-xs">profile</span>
                                 </Link>
@@ -34,7 +35,7 @@ function Navbar() {
                         }
                     </li>
                     <li className="relative px-5 py-3 md:hover:nav-hover">
-                        <NavLink to={"/cart"} className="flex flex-col">
+                        <NavLink to={"/cart"} className="flex flex-col ">
                             <ShoppingCartIcon className="h-6 w-6" />
                             <span className="absolute right-0 top-0 py-.5 px-2 rounded-full bg-red-600">{mealsQuantity}</span>
                             <span className="text-xs">cart</span>
