7ee94f3e45761eb72445ab10fa7d026112da0ce8
Bikash Kakati <bikashkakati796@gmail.com>
2024-01-29 10:55:19 +0530

    Style - some improvement

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -5,6 +5,7 @@ import { handleLogOut } from '../../services/redux/slices/authSlice';
 import Wrapper from '../../components/Ui/Wrapper';
 
 function Profile() {
+    const {currentUser} = useSelector(state => state.auth);
     const dispatch = useDispatch();
     const Navigate = useNavigate();
 
@@ -15,7 +16,8 @@ function Profile() {
     return (
         <div className="w-full h-full pt-20">
             <Wrapper>
-                <div className="max-w-[50rem] w-full min-h-32 bg-white shadow-lg m-auto flex items-center justify-center">
+                <div className="max-w-[50rem] w-full min-h-32 bg-white shadow-custom m-auto flex items-center justify-center flex-wrap">
+                    <p className="basis-full text-center font-semibold">{currentUser.email}</p>
                     <button
                         className="bg-red-500 hover:bg-red-600 text-white py-3 px-11"
                         onClick={handleLogout}
