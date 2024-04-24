f2dbc7bba2eb8d4dd2173e987744fd27c8b9d7e5
Bikash Kakati <bikashkakati796@gmail.com>
2024-04-24 08:47:15 +0530

    Style- Change loader style

--- src/components/Ui/Loader.jsx
+++ src/components/Ui/Loader.jsx
@@ -1,7 +1,7 @@
 export function Loader({initial=false}) {
     return (
         <div className={`${initial ? "h-dvh":"min-h-32"} w-full flex items-center justify-center`}>
-            <div className="w-16 h-16 border-8 border-t-zinc-600 animate-spin rounded-full"></div>
+            <div className="animate-spin inline-block size-12 border-[5px] border-current border-t-transparent text-zinc-600 rounded-full" role="status" aria-label="loading"></div>
         </div>
     )
 }
