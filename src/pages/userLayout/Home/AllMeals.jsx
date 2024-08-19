ff7aebcb9274ee7ba8f4b66392aba163d4ad4705
Bikash Kakati <bikashkakati796@gmail.com>
2024-08-19 20:12:54 +0530

    Fix: Loading check on wrong component

--- src/pages/userLayout/Home/AllMeals.jsx
+++ src/pages/userLayout/Home/AllMeals.jsx
@@ -6,72 +6,66 @@ import Wrapper from "../../../components/Ui/Wrapper";
 import { fetchApiData } from "../../../services/Api";
 
 function AllMeals() {
-    const [alphabet, setAlphabet] = useState(97);
-    const [mealData, setMealData] = useState([]);
-    const [loading, setLoading] = useState(false);
+  const [alphabet, setAlphabet] = useState(97);
+  const [mealData, setMealData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        setAlphabet(97);
-        fetchInitialData();
-    }, [])
+  useEffect(() => {
+    setAlphabet(97);
+    fetchInitialData();
+  }, []);
 
-    function fetchInitialData() {
-        setLoading(true);
-        fetchApiData(`search.php?f=${String.fromCharCode(alphabet)}`)
-            .then((res) => {
-                setMealData(res?.meals);
-                setAlphabet((prev) => prev + 1);
-                setLoading(false);
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    }
-    function fetchNextData() {
-        fetchApiData(`search.php?f=${String.fromCharCode(alphabet)}`)
-            .then((res) => {
-                if (mealData?.length) {
-                    return setMealData((prevData) => [...prevData, ...res?.meals]);
-                } else {
-                    return setMealData(res.meals);
-                }
-            })
-            .then(() => {
-                setAlphabet((prev) => prev + 1);
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    }
+  function fetchInitialData() {
+    setLoading(true);
+    fetchApiData(`search.php?f=${String.fromCharCode(alphabet)}`)
+      .then((res) => {
+        setMealData(res?.meals);
+        setAlphabet((prev) => prev + 1);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  function fetchNextData() {
+    fetchApiData(`search.php?f=${String.fromCharCode(alphabet)}`)
+      .then((res) => {
+        if (mealData?.length) {
+          return setMealData((prevData) => [...prevData, ...res?.meals]);
+        } else {
+          return setMealData(res.meals);
+        }
+      })
+      .then(() => {
+        setAlphabet((prev) => prev + 1);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    return (
-        <section className="text-black w-full mt-16">
-            <Wrapper>
-                <p className="text-3xl mb-4 font-normal">Find All Meals Here</p>
-                <div className="w-full">
-                    {loading &&
-                        <CardSkeletonMultiple/>
-                    }
-                    {
-                        !loading &&
-                        <InfiniteScroll
-                            className="flex pt-4 items-start justify-center flex-row flex-wrap gap-6"
-                            dataLength={mealData?.length || 0}
-                            next={fetchNextData}
-                            hasMore={alphabet <= 111}
-                            loader={<CardSkeletonMultiple/>}
-                        >
-                            {
-                                mealData?.map((meal) => {
-                                    return <Card key={meal.idMeal} mealData={meal} />
-                                })
-                            }
+  return (
+    <section className="text-black w-full mt-16">
+      <Wrapper>
+        <p className="text-3xl mb-4 font-normal">Find All Meals Here</p>
+        <div className="w-full">
+          {loading && <CardSkeletonMultiple />}
 
-                        </InfiniteScroll>
-                    }
-                </div>
-            </Wrapper>
-        </section>
-    )
+          <InfiniteScroll
+            className="flex pt-4 items-start justify-center flex-row flex-wrap gap-6"
+            dataLength={mealData?.length || 0}
+            next={fetchNextData}
+            hasMore={alphabet <= 111}
+            loader={<CardSkeletonMultiple />}
+          >
+            {!loading &&
+              mealData?.map((meal) => {
+                return <Card key={meal.idMeal} mealData={meal} />;
+              })}
+          </InfiniteScroll>
+        </div>
+      </Wrapper>
+    </section>
+  );
 }
 export default AllMeals;
