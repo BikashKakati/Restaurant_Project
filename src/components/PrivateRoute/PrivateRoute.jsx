22931209bb0adef2dfd980ea2b74bd61df6d5e8c
Bikash Kakati <118715700+BikashKakati@users.noreply.github.com>
2024-08-20 17:22:32 +0530

    Fix: Now stayes on current route if not login

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { setAuthModel } from '../../services/redux/slices/authSlice';
 
 function PrivateRoute({ children }) {
   const { currentUser } = useSelector(state => state.auth);
   const dispatch = useDispatch();
+  const location = useLocation();
   if(!currentUser){
     dispatch(setAuthModel(true));
-    return null;
+    return <Navigate to={location.pathname}/>;
   }
   return children;
 }
