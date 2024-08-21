41eb4725fbf3adacd1964276f3a442cee9444ab4
bikashkakati <bikashkakati796@gmail.com>
2024-08-21 11:05:04 +0530

    Fix: Redirect to previous route

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Navigate, useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { setAuthModel } from '../../services/redux/slices/authSlice';
 
 function PrivateRoute({ children }) {
   const { currentUser } = useSelector(state => state.auth);
   const dispatch = useDispatch();
   const location = useLocation();
+  const navigate  = useNavigate();
   if(!currentUser){
     dispatch(setAuthModel(true));
-    return <Navigate to={location.pathname}/>;
+    return navigate(-1);
   }
   return children;
 }
