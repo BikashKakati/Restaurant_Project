424487e446c0d273557947f10bf92bd48bd6889b
bikashkakati <bikashkakati796@gmail.com>
2024-08-22 17:25:18 +0530

    Fix: Dialog body

--- src/components/Ui/Dialog.jsx
+++ src/components/Ui/Dialog.jsx
@@ -4,9 +4,9 @@ import { useDispatch } from 'react-redux';
 export function Dialog({headerData, children, footerData, onCloseModel}) {
     return (
         <div tabIndex="-1" className="backdrop-blur-sm overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-full max-h-full">
-            <div className="relative p-4 w-full max-w-lg max-h-full">
-                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                    <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+            <div className="relative p-4 w-full max-w-lg h-full max-h-full">
+                <div className="relative bg-white rounded-lg shadow h-full dark:bg-gray-700">
+                    <div className="flex h-[15%] items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                         <h3 className="text-[2rem] font-semibold text-gray-900 dark:text-white">
                             {headerData}
                         </h3>
@@ -17,10 +17,10 @@ export function Dialog({headerData, children, footerData, onCloseModel}) {
 
                         </button>
                     </div>
-                    <div className="p-4 md:p-5">
+                    <div className="h-[70%]">
                         {children}
                     </div>
-                    <div className="flex items-center p-4 md:p-5 border-t rounded-b dark:border-gray-600">
+                    <div className="flex items-center h-[15%] p-4 md:p-5 border-t rounded-b dark:border-gray-600">
                         {footerData}
                     </div>
                 </div>
